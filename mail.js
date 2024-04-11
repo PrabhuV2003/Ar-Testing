04e41ee31e6f1c7081c79b95b8d79567019f6bab
PrabhuV2003 <prabhuofficial2003@gmail.com>
2024-04-11 18:44:00 +0530

    index.html file added

---
+++ mail.js
@@ -0,0 +1,54 @@
+const filterBtn = document.querySelectorAll('.filterBtn');
+const boxes = document.querySelectorAll('.box');
+const searchBox = document.querySelector('.search');
+
+searchBox.addEventListener('keyup',(e)=>{
+    searchText = e.target.value.toLowerCase().trim();
+
+    boxes.forEach((box)=>{
+        const data = box.dataset.item;
+        if(data.includes(searchText)) {
+            box.style.display = 'block';
+        } else {
+            box.style.display = 'none';
+        }
+    });
+
+    filterBtn.forEach((button)=>{
+        button.classList.remove('btn-clicked');
+      });
+      filterBtn[0].classList.add('btn-clicked');
+});
+
+filterBtn.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+  
+      e.preventDefault();
+      setActiveBtn(e);
+      const btnfilter = e.target.dataset.filter;
+      
+      boxes.forEach((box=>{
+        if(btnfilter=='all'){
+          box.style.display="block";
+        }else{
+  
+          const boxfilter=box.dataset.item;
+          if(btnfilter==boxfilter){
+            box.style.display="block";
+          }else{
+            box.style.display="none";
+          }
+          
+        }
+      }));
+  
+  
+    });
+  });
+
+  function  setActiveBtn(e){
+    filterBtn.forEach((button)=>{
+      button.classList.remove('btn-clicked');
+    });
+    e.target.classList.add('btn-clicked');
+  }
